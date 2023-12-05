79d66ddc57899a5ab7f1e8b242b52ef0303be834
noahkise <noah@kisefamily.com>
2023-12-05 09:04:29 -0800

    add three question pages
    Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
+import Question1 from './Question1';
+import Question2 from './Question2';
+import Question3 from './Question3';
 
 class TicketControl extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      counter: 1
     };
   }
 
   handleClick = () => {
     this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
+      counter: prevState.counter < 5 ? prevState.counter + 1 : 1
     }));
   }
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
-    if (this.state.formVisibleOnPage) {
+    if (this.state.counter === 1) {
+      currentlyVisibleState = <TicketList />;
+      buttonText = "Add Ticket";
+    } else if (this.state.counter === 2){
+      currentlyVisibleState = <Question1 />;
+      buttonText = "Yes";
+    } else if (this.state.counter === 3){
+      currentlyVisibleState = <Question2 />;
+      buttonText = "Yes";
+    } else if (this.state.counter === 4){
+      currentlyVisibleState = <Question3 />;
+      buttonText = "Yes";
+    } else if (this.state.counter === 5){
       currentlyVisibleState = <NewTicketForm />;
       buttonText = "Return to Ticket List";
     } else {
